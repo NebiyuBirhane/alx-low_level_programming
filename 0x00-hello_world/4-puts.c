494a5ee96d6b483173390a5c79f71680df4f9a54
NebiyuBirhane <nebiyubirhaneab@gmail.com>
2023-03-16 11:04:10 -0700

    Progrma

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
-/** 
-*main.using puts function to print lines
-*Return: 0(Success)
-*/
-int main(void){
-puts("\"Programming is like building a multilingual puzzle");
-return (0); 
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
