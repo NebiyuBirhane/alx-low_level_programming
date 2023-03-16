2232bcdc083ee9da19c75bb23f521c3115d2b496
NebiyuBirhane <nebiyubirhaneab@gmail.com>
2023-03-16 01:19:08 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/** 
+*main.using puts function to print lines
+*Return: 0(Success)
+*/
+int main(void){
+puts("\"Programing is like buliding a multilingular puzzel");
+return (0); 
+}
