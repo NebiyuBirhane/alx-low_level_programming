ac1dc4da28685bb454122434bcc8b2459e308bbb
NebiyuBirhane <nebiyubirhaneab@gmail.com>
2023-03-16 11:37:15 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
-#include <studio.h>
-/*
-*main.prints proper gramer using printf 
+#include <stdio.h>
+/**
+*main-Entry points 
 *RETURN:0(Succes)
+*
 */
-int main(void){
-printf("with proprer grammer ,but the outcome is a piece of art,""\n") 
-return (0);
+int main(void)
+{	printf("with proprer grammer, but the outcome is a piece of art,\n"); 
+	return (0);
 }
