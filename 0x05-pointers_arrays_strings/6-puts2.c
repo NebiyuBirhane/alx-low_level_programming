a36dcbfe50d04c86e9883dccec22758d3a45c4cb
NebiyuBirhane <nebiyubirhaneab@gmail.com>
2023-03-28 03:13:00 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts2 - function should print only one character
+ * @str: input
+ * REturn: print
+ */
+void puts2(char *str)
+{
+	int i = 0;
+	int t = 0;
+	char *y = str;
+	int o;
+	while (*y != '\0')
+	{
+		y++;
+		i++;
+	}
+	t = i - 1;
+	for (o = 0; o <= t; o++)
+	{
+		if (o % 2 == 0)
+		{
+			_putchar(str[o]);
+		}
+	}
+	_putchar('\n')
+}
+
