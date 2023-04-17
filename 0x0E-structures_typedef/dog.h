11181cf44336f217765102533df1f0a572f38df9
NebiyuBirhane <nebiyubirhaneab@gmail.com>
2023-04-17 08:39:52 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - short description
+ * @name: input value
+ * @age: input value
+ * @owner: input value
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
